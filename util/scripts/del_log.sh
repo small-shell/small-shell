4a33664bf484b87fc177f8b4d66a4cdbe47fe66e
Ken Naruo <ken.naruo@gmail.com>
2022-06-16 11:57:38 +0000

    improve log retention logic

--- util/scripts/del_log.sh
+++ util/scripts/del_log.sh
@@ -4,8 +4,14 @@
 SCRIPT_DIR=`dirname $0`
 . ${SCRIPT_DIR}/../../global.conf
 
-log_dir=${ROOT}/util/e-cron/log/joblog
+ecron_job=${ROOT}/util/e-cron/log/job
+ecron_message=${ROOT}/util/e-cron/log/message
+ecron_fileExchange=${ROOT}/util/e-cron/log/fileExchange
+audit=${ROOT}/users/audit
 
-find ${log_dir}/ -type f -mtime +180 | xargs rm -f
+find ${ecron_job}/ -type f -mtime +90 | xargs rm -f
+find ${ecron_message}/ -type f -mtime +90 | xargs rm -f
+find ${ecron_fileExchange}/ -type f -mtime +90 | xargs rm -f
+find ${audit}/ -type f -mtime +90 | xargs rm -f
 
 exit 0
