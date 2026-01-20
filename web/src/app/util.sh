11781981a57cb09eb8c0af2b2209b2ad06783f36
naruoken <ken.naruo@gmail.com>
2026-01-20 14:49:03 +0900

    modify sleep time of util script for Mac

--- web/src/app/util.sh
+++ web/src/app/util.sh
@@ -9,6 +9,6 @@ do
   sudo -u small-shell ${small_shell_path}/util/scripts/del_log.sh
   sudo -u small-shell ${small_shell_path}/util/scripts/rotate_srvlog.sh
 
-  # sleep 1 day
-  sleep 86400
+  # sleep 1 hour
+  sleep 3600
 done
