739abbf9bd690e7781e29be488c08619b87de422
naruoken <ken.naruo@gmail.com>
2026-02-27 14:13:02 +0900

    improve session delete logic for Web Apps

--- util/scripts/del_session.sh
+++ util/scripts/del_session.sh
@@ -7,8 +7,8 @@ SCRIPT_DIR=$(dirname $0)
 session_dir=${ROOT}/tmp/session
 que_session_dir=${ROOT}/tmp/que
 
-find ${session_dir}/ -type f -amin +240 | xargs rm -f 
-find ${session_dir}/ -type l -amin +240 | xargs rm -f 
-find ${que_session_dir}/ -type f -amin +240 | xargs rm -f 
+find ${session_dir}/ -type l -cmin +240 | xargs rm -f 
+find ${session_dir}/ -type f -cmin +240 | xargs rm -f 
+find ${que_session_dir}/ -type f -cmin +240 | xargs rm -f 
 
 exit 0
